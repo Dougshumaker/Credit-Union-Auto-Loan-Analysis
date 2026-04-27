506870fe0f3e6fa9788d9db3b4b62e5d66c4a702
Dougshumaker <douglashumaker@gmail.com>
2026-04-27 16:09:03 -0700

    Delete queries/queries/02_industry_delinquency_trend.sql

--- queries/queries/02_industry_delinquency_trend.sql
+++
@@ -1,6 +0,0 @@
-select auto_loans.cycle_date , 
-sum(new_vehicle_delinquency) as new_vehicle_delinquency,
-sum(used_vehicle_delinquency) as used_vehicle_delinquency , 
-sum(auto_loans.new_vehicle_delinquency) + sum(auto_loans.used_vehicle_delinquency) as total_delinquency from auto_loans
-group by cycle_date
-order by cycle_date desc
